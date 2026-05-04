f29bc4eddeba06c1dc7b7faf95149eecef2cbe01
krutesh08 <140951256+krutesh08@users.noreply.github.com>
2026-05-04 06:35:55 +0530

    Revert "Show invite-only events in plan switcher"
    This reverts commit 30fe1715140d208840db1104e2f35e5bf6035bd2.

--- src/AppFlow.tsx
+++ src/AppFlow.tsx
@@ -1414,7 +1414,7 @@ export default function App() {
           <EventDetailsOverlay
             event={selectedEvent}
             selectedCity={selectedCity}
-            allEvents={events}
+            allEvents={events.filter(e => !e.inviteOnly)}
             closeCalendarSignal={closeDetailsCalendarSignal}
             onCalendarVisibilityChange={setDetailsCalendarOpen}
             openPlanSwitcherSignal={openDetailsPlanSwitcherSignal}
